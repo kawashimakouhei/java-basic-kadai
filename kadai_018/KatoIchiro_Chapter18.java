53cc383d4a147f4bd0533be7013af7cffa5b5833
kawashimakouhei <70583268+kawashimakouhei@users.noreply.github.com>
2024-08-01 10:57:08 +0900

    課題18修正

--- kadai_018/KatoIchiro_Chapter18.java
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -2,12 +2,13 @@ package kadai_018;
 
 public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
 	
-	public void setGivenName(String name) {
-		this.givenName = name;	
+	public void setGivenName() {
+		this.givenName = "一郎";	
 	}
 	
 	public void eachIntroduce() {
 		System.out.println("好きな食べ物はリンゴです");
+		System.out.println("");
 	};
 
 }
